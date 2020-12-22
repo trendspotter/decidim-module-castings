292c363af020743c0e76faed31383cc7eb04246e
sergei-krylov <sergei.krylov@yahoo.com>
2020-12-22 15:44:10 +0300

    BODR-20 Fix starting computation process

--- app/commands/decidim/castings/admin/start_casting_processing.rb
+++ app/commands/decidim/castings/admin/start_casting_processing.rb
@@ -12,8 +12,8 @@ module Decidim
           return broadcast(:invalid, I18n.t("action_not_available_in_current_status", scope: "decidim.castings.admin.messages")) unless casting.can_start_processing?
           return broadcast(:invalid, I18n.t("no_data_present_to_start_processing", scope: "decidim.castings.admin.messages")) if casting.casting_data_rows.blank?
 
-          Decidim::Castings::ProcessCastingJob.perform_later(casting.id)
           casting.processing_scheduled_status!
+          Decidim::Castings::ProcessCastingJob.perform_later(casting.id)
 
           broadcast(:ok, casting)
         end
