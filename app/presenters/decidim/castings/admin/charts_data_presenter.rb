0f091c49c61c10ab0e55faeaec308ee211b4f9e9
sergei-krylov <sergei.krylov@yahoo.com>
2021-04-05 11:26:18 +0300

    BOSA21Q1-47 Result graphs - show % values with 2 decimals

--- app/presenters/decidim/castings/admin/charts_data_presenter.rb
+++ app/presenters/decidim/castings/admin/charts_data_presenter.rb
@@ -31,8 +31,8 @@ module Decidim
             rd = values.keys.map {|k| result.statistics.dig('candidates_attributes', attr, k) || 0 }
             sd_total = sd.inject(0, :+)
             rd_total = rd.inject(0, :+)
-            sd_percentages = values.keys.map { |k| (casting.data_source_statistics.dig('attributes', attr, k) * 100) / sd_total.round }
-            rd_percentages = values.keys.map { |k| (result.statistics.dig('candidates_attributes', attr, k) * 100) / rd_total.round }
+            sd_percentages = values.keys.map { |k| ((casting.data_source_statistics.dig('attributes', attr, k) * 100).to_f / sd_total).round(2) }
+            rd_percentages = values.keys.map { |k| ((result.statistics.dig('candidates_attributes', attr, k) * 100).to_f / rd_total).round(2) }
 
             data << {
               attribute: attr,
